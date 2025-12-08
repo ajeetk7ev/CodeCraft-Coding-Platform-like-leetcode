037660a3e2b9b600e76b8d10885583bb95941b5c
ajeetk7ev <ajeetk8568@gmail.com>
2025-12-08 14:30:21 +0530

    Added auth routes

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,13 +2,16 @@ import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
 import { dbConnect } from './config/db';
-
+import authRoutes from './routes/auth.routes'
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+
 app.get("/", (_, res) => {
       res.send("I am working fine")
 })
+
+app.use("/api/auth", authRoutes)
 
 app.listen(PORT, async() => {
     await dbConnect();
