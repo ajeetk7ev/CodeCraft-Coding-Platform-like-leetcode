a6a5cec4646fc748572ec283506e5fc5cb865790
ajeetk7ev <ajeetk8568@gmail.com>
2025-12-08 14:24:28 +0530

    Added login controller

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -5,11 +5,10 @@ import { signupSchema, loginSchema } from "../validations/auth.schema";
 import { generateToken } from "../utils/generateToken";
 import { error } from "console";
 
-
 export const signup = async (req: Request, res: Response) => {
   try {
     const parsed = signupSchema.safeParse(req.body);
-   if (!parsed.success) {
+    if (!parsed.success) {
       return res
         .status(400)
         .json({ success: false, errors: parsed.error.flatten().fieldErrors });
@@ -19,7 +18,9 @@ export const signup = async (req: Request, res: Response) => {
 
     const existingUser = await User.findOne({ $or: [{ email }, { username }] });
     if (existingUser) {
-      return res.status(400).json({success:false, message: "Email or Username already exists" });
+      return res
+        .status(400)
+        .json({ success: false, message: "Email or Username already exists" });
     }
 
     const hashedPass = await bcrypt.hash(password, 10);
@@ -27,23 +28,50 @@ export const signup = async (req: Request, res: Response) => {
     const user = await User.create({
       username,
       email,
-      password: hashedPass
+      password: hashedPass,
     });
 
     const token = generateToken(user._id);
 
     return res.status(201).json({
-      success:true,
+      success: true,
       message: "Signup successful",
       user: { id: user._id, username: user.username, email: user.email },
-      token
+      token,
     });
-
   } catch (err) {
     console.log("Error in signup", error);
-    return res.status(500).json({success:false, message: "Server error" });
+    return res.status(500).json({ success: false, message: "Server error" });
   }
 };
 
+export const login = async (req: Request, res: Response) => {
+  try {
+    const parsed = loginSchema.safeParse(req.body);
+    if (!parsed.success) {
+      return res
+        .status(400)
+        .json({ success: false, errors: parsed.error.flatten().fieldErrors });
+    }
 
+    const { email, password } = parsed.data;
 
+    const user = await User.findOne({ email });
+    if (!user) return res.status(404).json({success:false, message: "User not found" });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch)
+      return res.status(400).json({success:false, message: "Invalid credentials" });
+
+    const token = generateToken(user._id);
+
+    return res.json({
+      message: "Login successful",
+      user: { id: user._id, username: user.username, email: user.email },
+      token,
+    });
+  } catch (err) {
+    console.log("Error in login", err);
+    return res.status(500).json({success:false, message: "Server error" });
+  }
+};
