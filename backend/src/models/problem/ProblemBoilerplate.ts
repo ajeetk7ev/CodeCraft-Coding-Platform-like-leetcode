a7b5a5b73e409c624086e2d92bbb5e1614a11530
ajeetk7ev <ajeetk8568@gmail.com>
2025-12-16 16:35:15 +0530

    update ProblemBoilerplate model

--- backend/src/models/problem/ProblemBoilerplate.ts
+++ backend/src/models/problem/ProblemBoilerplate.ts
@@ -3,15 +3,37 @@ import mongoose, { Schema, Document, Types } from "mongoose";
 export interface IProblemBoilerplate extends Document {
   problem: Types.ObjectId;
   language: string;
-  template: string;
+
+  // Code shown to user (ONLY function)
+  userTemplate: string;
+
+  // Hidden boilerplate (imports + main + test runner)
+  fullTemplate: string;
 }
 
 const problemBoilerplateSchema = new Schema<IProblemBoilerplate>(
   {
-    problem: { type: Schema.Types.ObjectId, ref: "Problem", required: true },
+    problem: {
+      type: Schema.Types.ObjectId,
+      ref: "Problem",
+      required: true,
+    },
 
-    language: { type: String, required: true },
-    template: { type: String, required: true },
+    language: {
+      type: String,
+      required: true,
+      enum: ["cpp", "java", "python", "javascript"],
+    },
+
+    userTemplate: {
+      type: String,
+      required: true,
+    },
+
+    fullTemplate: {
+      type: String,
+      required: true,
+    },
   },
   { timestamps: true }
 );
